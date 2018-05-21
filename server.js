310d83190c994dbab8e5d336810c21efdab9f7a8
Justin <justinwhitaker2016@gmail.com>
2018-05-21 15:19:24 -0700

    Added a test route. Most changes since last commit have occurred in the client repo.

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ client.on('error', err => console.error(err));
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
-
+app.get('/test', (req,res) => res.send('Hello, world!'));
 app.get('*', (req,res) => res.status(403).send('This route does not exist.'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
